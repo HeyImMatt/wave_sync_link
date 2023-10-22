9f251bf8aba9745e210d8bcb8ce5858d7ce06df2
heyimmatt <mrmcfarland@gmail.com>
2023-10-22 12:32:01 -0700

    Use raw input stream and add timeout
    Using raw input stream since numpy not available

--- src/wave_sync_link.py
+++ src/wave_sync_link.py
@@ -44,8 +44,8 @@ def button_pressed_handler():
     print("Button held. Recording audio.")
     global wave_to_send
     wave_to_send = None  # Initialize the variable
-    with sd.InputStream(callback=record_audio, channels=1, samplerate=fs):
-        button.wait_for_release()  # Wait until the button is released
+    with sd.RawInputStream(callback=record_audio, channels=1, samplerate=fs):
+        button.wait_for_release(15)  # Wait until the button is released
     if wave_to_send is not None:
         print("Recording stopped. Writing to file.")
         sf.write(os.path.join(path, 'wave_to_send.wav'), wave_to_send, fs)
