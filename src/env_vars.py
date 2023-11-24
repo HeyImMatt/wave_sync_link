0d26e863ed41446bb760ad19c3ac8377ac6fe01e
heyimmatt <mrmcfarland@gmail.com>
2023-11-24 11:08:26 -0800

    Create env vars file

---
+++ src/env_vars.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HOME_PATH = os.path.expanduser("~")
+DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH")
+UPLOAD_PATH = os.environ.get("UPLOAD_PATH")
+FIREBASE_KEY_PATH = os.environ.get("FIREBASE_KEY_PATH")
+STORAGE_BUCKET = os.environ.get("STORAGE_BUCKET")
+PROJECT_NAME = os.environ.get("PROJECT_NAME")
+SUBSCRIPTION_NAME = os.environ.get("SUBSCRIPTION_NAME")
+SENDER_NAME = os.environ.get("SENDER_NAME")
